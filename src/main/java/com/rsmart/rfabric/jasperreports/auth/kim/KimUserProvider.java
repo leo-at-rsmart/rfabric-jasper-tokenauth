6561c91c420ea0ba185e420f7f72bb57f365c9c6
Przybylski 중광 <r351574nc3@gmail.com>
2012-12-12 03:16:37 -0700

    Updating KimUserProvider to use the KIM Identity Service

--- src/main/java/com/rsmart/rfabric/jasperreports/auth/kim/KimUserProvider.java
+++ src/main/java/com/rsmart/rfabric/jasperreports/auth/kim/KimUserProvider.java
@@ -1,36 +1,107 @@
 package com.rsmart.rfabric.jasperreports.auth.kim;
 
+import org.kuali.rice.kim.v2_0.*;
+
 import org.springframework.security.GrantedAuthority;
 
 import com.rsmart.rfabric.jasperreports.auth.ExternalUserProvider;
 
 public class KimUserProvider implements ExternalUserProvider {
+    protected IdentityService_Service identityService;
+    protected RoleService_Service roleService;
+    protected PermissionService_Service permissionService;
 
-  public boolean userExists(String user) {
-    // TODO Consult KIM to get the real answer
-    return true;
-  }
 
-  @SuppressWarnings("serial")
-  public GrantedAuthority[] getAuthoritiesForUser(String user) {
-    // TODO Determine what roles the user should have based upon roles in KIM
+    public boolean userExists(String user) {
+        return getKimIdentityService().getPrincipalByPrincipalName(user) != null;
+    }
+
+    @SuppressWarnings("serial")
+    public GrantedAuthority[] getAuthoritiesForUser(String user) {
+        // TODO Determine what roles the user should have based upon roles in KIM
     
-    // for now this is just a mock implementaiton that gives the user admin role
-    GrantedAuthority[] mockAuthorities = new GrantedAuthority[1];
+        // for now this is just a mock implementaiton that gives the user admin role
+        GrantedAuthority[] mockAuthorities = new GrantedAuthority[1];
     
-    mockAuthorities[0] = new GrantedAuthority() {
+        mockAuthorities[0] = new GrantedAuthority() {
 
-      public int compareTo(Object o) {
-        GrantedAuthority that = (GrantedAuthority)o;
-        return getAuthority().compareTo(that.getAuthority());
-      }
+                public int compareTo(Object o) {
+                    GrantedAuthority that = (GrantedAuthority)o;
+                    return getAuthority().compareTo(that.getAuthority());
+                }
 
-      public String getAuthority() {
-        return "ROLE_ADMIN";
-      }
+                public String getAuthority() {
+                    return "ROLE_ADMIN";
+                }
       
-    };
-    return mockAuthorities;
-  }
+            };
+        return mockAuthorities;
+    }
 
+    public IdentityService getKimIdentityService() {
+        return getIdentityService().getIdentityServicePort();
+    }
+        
+    /**
+     * Gets the value of identityService
+     *
+     * @return the value of identityService
+     */
+    public IdentityService_Service getIdentityService() {
+        return this.identityService;
+    }
+
+    /**
+     * Sets the value of identityService
+     *
+     * @param argIdentityService Value to assign to this.identityService
+     */
+    public void setIdentityService(final IdentityService_Service argIdentityService) {
+        this.identityService = argIdentityService;
+    }
+
+    public RoleService getKimRoleService() {
+        return getRoleService().getRoleServicePort();
+    }
+
+    /**
+     * Gets the value of roleService
+     *
+     * @return the value of roleService
+     */
+    public RoleService_Service getRoleService() {
+        return this.roleService;
+    }
+
+    /**
+     * Sets the value of roleService
+     *
+     * @param argRoleService Value to assign to this.roleService
+     */
+    public void setRoleService(final RoleService_Service argRoleService) {
+        this.roleService = argRoleService;
+    }
+
+    
+    public PermissionService getKimPermissionService() {
+        return getPermissionService().getPermissionServicePort();
+    }
+
+    /**
+     * Gets the value of permissionService
+     *
+     * @return the value of permissionService
+     */
+    public PermissionService_Service getPermissionService() {
+        return this.permissionService;
+    }
+
+    /**
+     * Sets the value of permissionService
+     *
+     * @param argPermissionService Value to assign to this.permissionService
+     */
+    public void setPermissionService(final PermissionService_Service argPermissionService) {
+        this.permissionService = argPermissionService;
+    }
 }
